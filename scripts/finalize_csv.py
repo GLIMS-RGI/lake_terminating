fa1b61890af0a9ad91dd5df66351521c9c591dac
Bob McNabb <robertmcnabb@gmail.com>
2024-05-22 23:16:17 +0100

    make clear that CSV can be used and change how output filename is generated; drop null lake_level values

--- scripts/finalize_csv.py
+++ scripts/finalize_csv.py
@@ -11,7 +11,7 @@ def _argparser():
     parser = argparse.ArgumentParser(description=helpstr,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('fn_outlines', action='store', type=str,
-                        help='The filename of the outlines shapefile to convert.')
+                        help='The filename of the outlines shapefile or CSV to convert.')
     parser.add_argument('contributor', action='store', type=str,
                         help='The surname of the contributor who did the mapping.')
     parser.add_argument('-doi', action='store', type=str, default=None,
@@ -23,7 +23,8 @@ def main():
     parser = _argparser()
     args = parser.parse_args()
 
-    fn_out = os.path.basename(args.fn_outlines).split('.shp')[0] + f'_{(args.contributor.lower())}.csv' # the name of the csv file to write out
+    fn_out = '_'.join([os.path.splitext(os.path.basename(args.fn_outlines))[0],
+                       args.contributor.lower() + '.csv'])  # the name of the csv file to write out
 
     outlines = gpd.read_file(args.fn_outlines)
     # set image date using the image id - currently assumes that the img_id is a landsat product id
@@ -34,10 +35,14 @@ def main():
     if args.doi is not None:
         outlines['inv_doi'] = args.doi
 
+    # drop any null lake_level values, then ensure that this column is an integer
+    outlines.dropna(subset=['lake_level'], inplace=True)
     outlines['lake_level'] = outlines['lake_level'].astype(int)
 
     # change the column names since we're not bound by esri rules with the csv
-    outlines.rename(columns={'term_type': 'auto_term', 'inv_doi': 'inventory_doi', 'cont': 'contributor', 'img_id': 'image_id', 'img_date': 'image_date'}, inplace=True)
+    outlines.rename(columns={'term_type': 'auto_term', 'inv_doi': 'inventory_doi',
+                             'cont': 'contributor', 'img_id': 'image_id',
+                             'img_date': 'image_date'}, inplace=True)
 
     # remove the geometry column before writing to csv
     colnames = list(outlines.columns)
